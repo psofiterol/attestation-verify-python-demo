6e40731d2d0a8ac594a70d6cb2139611e82bc7b3
psofiterol <gpentafragkas@protonmail.com>
2025-07-17 15:01:09 +0300

    chore: sign_params edit

--- app_sign_and_verify.py
+++ app_sign_and_verify.py
@@ -35,15 +35,14 @@ def verify_signature(sign_params: str, signature: str) -> str:
 
 if __name__ == '__main__':
     # Example usage
-    # Replace with your own app secret, this key just for test: appId->0x811169961c2949e8c91e7840c5452cc4deb1942c
+    # Replace with your own app secret, this key just for test: appId->0x17ae11d76b72792478d7b7bcdc76da9574ab3cf8
     appSecret = "0xafa01caf44f07d2b21bc5e2bde1de2a8ba56f33ac2e223169f99634f57d049b5"
     # !!!Notice: sign_params is a string not object
-    # sign_params = "{\"appId\":\"0x811169961c2949e8c91e7840c5452cc4deb1942c\",\"attTemplateID\":\"2e3160ae-8b1e-45e3-8c59-426366278b9d\",\"userAddress\":\"0xB12a1f7035FdCBB4cC5Fa102C01346BD45439Adf\",\"timestamp\":1752730452083,\"attMode\":{\"algorithmType\":\"proxytls\",\"resultType\":\"plain\"}}"
-    sign_params = {"appId":"0x17ae11d76b72792478d7b7bcdc76da9574ab3cf8","attTemplateID":"369e1db8-47c9-4dc6-85b5-037cd02d3383","userAddress":"0x7ab44DE0156925fe0c24482a2cDe48C465e47573","timestamp":1752746994843,"attMode":{"algorithmType":"proxytls","resultType":"plain"},"requestid":"89cfef86-c272-410e-b66e-f38d67916852","backUrl":"","computeMode":"nonecomplete"}
+    sign_params = "{\"appId\":\"0x17ae11d76b72792478d7b7bcdc76da9574ab3cf8\",\"attTemplateID\":\"369e1db8-47c9-4dc6-85b5-037cd02d3383\",\"userAddress\":\"0x7ab44DE0156925fe0c24482a2cDe48C465e47573\",\"timestamp\":1752746994843,\"attMode\":{\"algorithmType\":\"proxytls\",\"resultType\":\"plain\"},\"requestid\":\"89cfef86-c272-410e-b66e-f38d67916852\",\"backUrl\":\"\",\"computeMode\":\"nonecomplete\"}"
     print("sign_params:", sign_params)
-    sig = sign_message(appSecret, json.dumps(sign_params))
+    sig = sign_message(appSecret, sign_params)
     print("Signature:", "0x"+sig)
-    recovered = verify_signature(json.dumps(sign_params), sig)
+    recovered = verify_signature(sign_params, sig)
     print("Recovered appId:", recovered)
     signedRequestStr = {
         "attRequest": sign_params,
